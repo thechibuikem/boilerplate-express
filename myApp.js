fdf5b2f166936d6397053dd2a664216632dcec89
Chukwuemeka David <136800623+thechibuikem@users.noreply.github.com>
2025-09-30 14:27:51 +0000

    before I switched to practice on local

--- myApp.js
+++ myApp.js
@@ -1,47 +1,30 @@
-let express = require('express');
+require("dotenv").config();
+
+let express = require("express");
 let app = express();
 
-// Serve static files from public folder
-app.use('/public', express.static(__dirname + '/public'));
+// // Serve static files from public folder
+app.use("/public", express.static(__dirname + "/public"));
+
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
 
 // Serve index.html at root
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// Serve index.html at root
+app.get("/json", function (req, res) {
+  let variable = process.env.MESSAGE_STYLE;
+  if (variable == "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
+    return;
+  }
+  res.json({ message: "Hello json" });
+});
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
