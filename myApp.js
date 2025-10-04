d6c9193e69a8d212755385854f25ee8f1c6f0301
Chukwuemeka David <136800623+thechibuikem@users.noreply.github.com>
2025-10-04 13:05:25 +0000

    finished express

--- myApp.js
+++ myApp.js
@@ -1,9 +1,51 @@
 require("dotenv").config();
-
+const bodyParser = require("body-parser");
+// const { json } = require("body-parser");
 let express = require("express");
 let app = express();
 
-// // Serve static files from public folder
+// practicing using body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// reading from html form
+app.post("/name", function (req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}` });
+});
+
+// practicing query parameters and verb chaining
+// app
+//   .route("/name")
+//   .get(function (req, res) {
+//     res.json({ name: `${req.query.first} ${req.query.last}` });
+//   })
+//   .post(function (req, res) {
+//     res.json({ name: `${req.body.first} ${req.body.last}` });
+//   });
+
+// creating an echo server
+// app.get("/:word/echo", function (req, res) {
+//   res.json({ echo: req.params.word });
+// });
+
+// // creating and chaining middleware
+// app.get(
+//   "/now",
+//   function (req, res, next) {
+//     req.time = new Date().toString();
+//     next();
+//   },
+//   function (req, res) {
+//     res.json({ time: req.time });
+//   },
+// );
+
+// // creating and giving my middleware logger access
+// app.use(function (req, res, next) {
+//   console.log(`${req.method} ${req.path} - ${req.ip}`);
+//   next();
+// });
+
+// Serve static files from public folder
 app.use("/public", express.static(__dirname + "/public"));
 
 // app.get("/", function (req, res) {
@@ -15,16 +57,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Serve index.html at root
-app.get("/json", function (req, res) {
-  let variable = process.env.MESSAGE_STYLE;
-  if (variable == "uppercase") {
-    res.json({ message: "Hello json".toUpperCase() });
-    return;
-  }
-  res.json({ message: "Hello json" });
-});
+// //
+// app.get("/json", function (req, res) {
+//   let variable = process.env.MESSAGE_STYLE;
+//   if (variable == "uppercase") {
+//     res.json({ message: "Hello json".toUpperCase() });
+//     return;
+//   }
+//   res.json({ message: "Hello json" });
+// });
 
-console.log("Hello World");
+// console.log("Hello World");
 
 module.exports = app;
